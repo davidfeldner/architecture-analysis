a8182780eb3f942bdccdca1705bbe6a665413a89
davidfeldner <davidfeldner12@gmail.com>
2026-05-08 14:35:03 +0200

    Many images with limit and weight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from numpy import add, shape
 import tree_sitter_typescript as ts_typescript
 from tree_sitter import Language, Parser, Query, QueryCursor
 
@@ -11,6 +12,7 @@ import matplotlib
 
 import git
 import os
+from collections import defaultdict
 
 matplotlib.use("TkAgg")
 
@@ -40,19 +42,55 @@ def dependencies_graph(code_root_folder):
     return G
 
 
-def draw_graph(G, size, **args):
-    net = Network(height="750px", width="100%")
+def make_graph():
+    net = Network(directed=True, height="750px", width="100%")
 
-    net.from_nx(G)
-    net.show_buttons(filter_=["physics"])
+    net.set_options("""
+        var options = {
+            "physics": {
+                "enabled": false
+            },
+            "edges": {
+                "smooth": {
+                    "enabled": false
+                }
+            }
+        }
+        """)
+    return net
+
+
+def draw_graph(net):
+
+    # net.from_nx(G)
+    # net.show_buttons()
+    # net.barnes_hut(overlap=0.5, spring_strength=0.01, spring_length=500, gravity=-8000)
     net.show("graph.html", notebook=False)
 
 
-def dependencies_digraph(code_root_folder):
+def add_node(net, name, color):
+    net.add_node(
+        name,
+        color=color,
+        shape="box",
+        # size=0,
+        # mass=1,
+        font={
+            # "size": 40,
+            "color": color,
+            "background": "#eef2ff",
+            "strokeWidth": 0.2,
+            "strokeColor": "#ffffff",
+        },
+    )
+
+
+def dependencies_digraph(net, code_root_folder):
     files = Path(code_root_folder).rglob("*.ts")
+    min_weight = 0
 
-    G = nx.DiGraph()
-
+    dirs = set()
+    references = defaultdict(lambda: defaultdict(int))
     for file in files:
         file_path = str(file)
 
@@ -61,15 +99,53 @@ def dependencies_digraph(code_root_folder):
         if not applyFilters(source_module):
             continue
 
-        if source_module not in G.nodes:
-            G.add_node(source_module)
+        depth = 2
+        source_parts = source_module.split("/")
+        if source_parts[-1] == "index":
+            source_parts.pop(-1)
+            source_module = "/".join(source_parts)
+
+        if len(source_parts) <= depth or source_module.startswith("@"):
+            # add_node(net, source_module, "#2255aa")
+            pass
+        else:
+            source_module = "/".join(source_parts[:depth]) + "/"
+            add_node(net, source_module, "#44bb44")
 
         for target_module in imports_from_file(file_path):
+            print(net.nodes)
 
-            G.add_edge(source_module, target_module)
-            # print(module_name + "=>" + each + ".")
+            target_parts = target_module.split("/")
+            if len(target_parts) > depth:
+                target_module = "/".join(target_parts[:depth]) + "/"
+            references[source_module][target_module] += 1
 
-    return G
+        # print(module_name + "=>" + each + ".")
+    print(references)
+    for s in references:
+        for t in references[s]:
+            if s == t:
+                continue
+            print(s, " : ", t)
+            try:
+                weight = references[s][t]
+                if weight < min_weight:
+                    continue
+                net.add_edge(
+                    s,
+                    t,
+                    label=str(references[s][t]),
+                    # font={"size": 20},
+                    # width=0.5 + weight * 0.2,
+                    # arrows={
+                    #     "to": {
+                    #         "enabled": True,
+                    #         "scaleFactor": 0.5,  # keeps arrowhead small regardless of width
+                    #     }
+                    # },
+                )
+            except:
+                pass
 
 
 def module_name_from_file_path(full_path):
@@ -129,10 +205,11 @@ def imports_from_file(filename: str):
 
 
 def main():
+    net = make_graph()
     if not os.path.exists(CODE_ROOT_FOLDER):
         get_source()
-    DG = dependencies_digraph(CODE_ROOT_FOLDER)
-    draw_graph(DG, (40, 40), with_labels=True)
+    dependencies_digraph(net, CODE_ROOT_FOLDER)
+    draw_graph(net)
 
 
 if __name__ == "__main__":
